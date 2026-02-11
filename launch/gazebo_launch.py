750f750c74bb3c81d4b24ac1dafdd654019b0003
anthony-som <qiko0425@gmail.com>
2026-02-11 01:20:25 -0500

    add models

--- launch/gazebo_launch.py
+++ launch/gazebo_launch.py
@@ -1,18 +1,20 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    # 1. Get the path to your package and world file
-    pkg_path = get_package_share_directory('template_world') # Double check this name!
+    pkg_path = get_package_share_directory('template_world')
     world_file_path = os.path.join(pkg_path, 'worlds', 'template.world')
+    models_path = os.path.join(pkg_path, 'models')
 
-    # 2. Get the gazebo_ros package path for the empty_world launch
     gazebo_ros_path = get_package_share_directory('gazebo_ros')
 
-    # 3. Create the Gazebo launch action
+    # Append our models directory to GAZEBO_MODEL_PATH
+    existing = os.environ.get('GAZEBO_MODEL_PATH', '')
+    model_path = models_path + ':' + existing if existing else models_path
+
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_path, 'launch', 'gazebo.launch.py')
@@ -20,7 +22,7 @@ def generate_launch_description():
         launch_arguments={'world': world_file_path}.items()
     )
 
-    # 4. RETURN the LaunchDescription (Crucial step!)
     return LaunchDescription([
+        SetEnvironmentVariable('GAZEBO_MODEL_PATH', model_path),
         gazebo
     ])
